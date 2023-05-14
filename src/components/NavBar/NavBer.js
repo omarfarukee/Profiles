f80873e057ebb26a0e62a059a6683867e2ef8592
omarfarukee <umarfaruque1920@gmail.com>
2023-05-14 17:07:45 +0600

    ber done

--- src/components/NavBar/NavBer.js
+++ src/components/NavBar/NavBer.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import pic from '../../images/pngwing.com.png'
 import { FaHome, FaProjectDiagram, FaUserCog, FaPhone, FaFilePdf } from 'react-icons/fa';
 import './NavBer.css'
 import { TypeAnimation } from 'react-type-animation';
@@ -33,14 +34,13 @@ const NavBer = () => {
                 {/* <div><button className='text-3xl bg-primary rounded p-1 w-40'> Resume</button></div> */}
             </div>
 
-
-            <div className='flex justify-center mt-36'>
+            <div className='mt-32'>
                 <div className='flex justify-between dev-width'>
-                    <div className='first-div'>
-                        <h1 className='text-3xl text-green-500 font-extrabold text-white'>
+                    <div className='first-div ml-28'>
+                        <h1 className='text-6xl text-green-500 font-extrabold text-white'>
                             <TypeAnimation
                                 sequence={[
-                                    'Hello hiiiii',
+                                    'Hello',
                                     1500,
                                     'I am react developer',
                                     1000
@@ -54,43 +54,12 @@ const NavBer = () => {
                         <p className='text-white mt-3'>
                             <TypeAnimation
                                 sequence={[
-                                    " Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic",
+                                    "Becoming a good minded developer requires the right intention, hard work and practice. So for almost a long year I was able to identify myself as a Full-Stack web developer by adopting that consistency. I have learned a lot in my journey as a developer and am still learning and will learn in the future.",
                                     1500,
                                     // 'I am react developer',
                                     // 1000
                                 ]}
-                                speed={10} // Custom Speed from 1-99 - Default Speed: 40
-                                style={{ fontSize: '1em' }}
-                                wrapper="span" // Animation will be rendered as a <span>
-                                repeat={Infinity} // Repeat this Animation Sequence infinitely
-                            />
-
-                        </p>
-                    </div>
-                    <div className='first-div'>
-                        <h1 className='text-3xl font-bold text-green-500 text-white'>
-                            <TypeAnimation
-                                sequence={[
-                                    'Hello hiiiii',
-                                    1500,
-                                    'I am react developer',
-                                    1000
-                                ]}
-                                speed={10} // Custom Speed from 1-99 - Default Speed: 40
-                                style={{ fontSize: '1em' }}
-                                wrapper="span" // Animation will be rendered as a <span>
-                                repeat={Infinity} // Repeat this Animation Sequence infinitely
-                            />
-                        </h1>
-                        <p className='text-white mt-3'>
-                            <TypeAnimation
-                                sequence={[
-                                    " Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic",
-                                    1500,
-                                    // 'I am react developer',
-                                    // 1000
-                                ]}
-                                speed={10} // Custom Speed from 1-99 - Default Speed: 40
+                                speed={75} // Custom Speed from 1-99 - Default Speed: 40
                                 style={{ fontSize: '1em' }}
                                 wrapper="span" // Animation will be rendered as a <span>
                                 repeat={Infinity} // Repeat this Animation Sequence infinitely
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,24 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Projects.css'
 const Projects = () => {
+    const [color, setColor] = useState(false)
+
+    const changeBackground = () => {
+        if(window.scrollY >= 200){
+            setColor(true)
+        }
+        else{
+            setColor(false)
+        }
+    }
+    window.addEventListener('scroll', changeBackground)
     return (
-        <div>
+        <div className={color ? 'active projects-backs': 'projects-backs'}>
+            <div>
+             <h1 className='text-2xl'>hellooooooo</h1>
+             <h1 className='text-2xl'>hellooooooo</h1>
+             <h1 className='text-2xl'>hellooooooo</h1>
+             <h1 className='text-2xl'>hellooooooo</h1>
+             <h1 className='text-2xl'>hellooooooo</h1>
+             <h1 className='text-2xl'>hellooooooo</h1>
+             <h1 className='text-2xl'>hellooooooo</h1>
+             <h1 className='text-2xl'>hellooooooo</h1>
+             <h1 className='text-2xl'>hellooooooo</h1>
+             <h1 className='text-2xl'>hellooooooo</h1>
+             <h1 className='text-2xl'>hellooooooo</h1>
             <h1 className='text-2xl'>hellooooooo</h1>
             <h1 className='text-2xl'>hellooooooo</h1>
-            <h1 className='text-2xl'>hellooooooo</h1>
-            <h1 className='text-2xl'>hellooooooo</h1>
-            <h1 className='text-2xl'>hellooooooo</h1>
-            <h1 className='text-2xl'>hellooooooo</h1>
-            <h1 className='text-2xl'>hellooooooo</h1>
-            <h1 className='text-2xl'>hellooooooo</h1>
-            <h1 className='text-2xl'>hellooooooo</h1>
-            <h1 className='text-2xl'>hellooooooo</h1>
-            <h1 className='text-2xl'>hellooooooo</h1>
-            <h1 className='text-2xl'>hellooooooo</h1>
-            <h1 className='text-2xl'>hellooooooo</h1>
-            <h1 className='text-2xl'>hellooooooo</h1>
-            <h1 className='text-2xl'>hellooooooo</h1>
-            <h1 className='text-2xl'>hellooooooo</h1>
+            </div>
+          
+            
         </div>
     );
 };
